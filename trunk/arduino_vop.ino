ff1b2a851c8c2b7b00aecb667b81ffc86c3886b8
douglaskippsmith@gmail.com <douglaskippsmith@gmail.com@6b01f2e6-1a7c-f450-ca5f-7ab2dec3552b>
2013-09-19 18:27:54 +0000

    Ironing out command handler, and error handling

--- trunk/arduino_vop.ino
+++ trunk/arduino_vop.ino
@@ -25,7 +25,7 @@
 #define PIN_RASPI_TRANSISTOR_REROUTE 7
 #define PIN_DEBUG_LED 13
 
-byte test = 0; 
+byte test = 1; 
 
 // ----------------------------------------
 // -- Command Buffer & Command variables --
@@ -38,103 +38,175 @@ byte test = 0;
 // 4th Byte: 0x0A, the end of the command.
 
 // What's the maximum index for the param buffer? (We count the command, plus two parameters, which is 3. Excludes the end of a command.)
-#define PARAMETERS_MAX 3
-#define END_OF_COMMAND 0x0A
-unsigned char command = 0;
-unsigned char param_buffer[2];		// The two posible bytes for the command parameters.
-unsigned char param_index = 0;		// The Index for writing to the buffer.
-bool command_error_flag = false; 	// Denotes an error while reading a command.
+#define MAX_COMMAND_PARAMETERS 3
+#define END_OF_COMMAND 10
+byte command = 1;			// The issued command.
+byte param_buffer[2];		// The two posible bytes for the command parameters.
+byte command_complete = 1;	// Did we finish getting the command?
+
+// ----------------------------------------
+// -- Command definitions -----------------
+// ----------------------------------------
+// These are the possible commands 
+// you can issue.
+
+#define CMD_GET_IGNITION_STATE 11
+#define CMD_GET_LAST_IGNITION_CHANGE 12
+
+// ----------------------------------------
+// -- Error Definitions -------------------
+// ----------------------------------------
+// Errors, they happen. 
+// We define the possibilities here.
+
+byte error_flag = 1; 	// Denotes an error
+
+#define ERR_BUFFER_OVERFLOW 1
+#define ERR_COMMAND_UNKNOWN 2
+#define ERR_COMMAND_INCOMPLETE 3
+
+
 
 // ----------------------------------------
 // -- Result Buffer & Result varaibles ----
 // ----------------------------------------
 
-unsigned int result = 2;
 
 
-// the setup routine runs once when you press reset:
-void setup() {
+// --------------------------------------------------------------------------
+// -- fillRequest: What happens when there's a request from the i2c master.
+// Which really means, handling the command that was read in receiveData()
 
-	// initialize the pin to turn the relay on, as an output.
-	pinMode(PIN_RASPI_RELAY, OUTPUT);
+void fillRequest() {
 
-	// Here's out debug LED, it's an output.
-	pinMode(PIN_DEBUG_LED, OUTPUT);
+	Serial.println("Filling request."); 
 
-	// listen on the ignition, as input
-	pinMode(PIN_IGNITION, INPUT);
+	// Let's test with an int.
+	unsigned int result = 0;
+	
+	if (command_complete == 1) {
+		// -- Command handler.
+		if (error_flag == 0) {
+			
+			// No error at this point.
+			switch (command) {
+				case CMD_GET_IGNITION_STATE:
+					result = 2600;
+					break;
 
-	if (DEBUG) {
-	  Serial.begin(9600);
+				case CMD_GET_LAST_IGNITION_CHANGE:
+					result = 420;
+					break;
+
+				default:
+					// Command is unknown.
+					result = 0;
+					error_flag = ERR_COMMAND_UNKNOWN;
+					break;
+			}
+
+		} else {
+			// Ok, there's an error here. We won't try to handle the command.
+			// Let's set the result to 0.
+			result = 0;
+		}
+	} else {
+
+		// We never completely got that command.
+		// Chances are you'll see the byte that's wrong as the "command" byte in the return. 
+		error_flag = ERR_COMMAND_INCOMPLETE;
+
 	}
 
-	Wire.begin(I2C_ADDRESS);	
-	Wire.onReceive(receiveData);
-	Wire.onRequest(fillRequest);
-  
+	// Let's just echo back the command and params now.
+	// byte writer[] = {command,param_buffer[0],param_buffer[1]};
+
+	// Go ahead and convert that integer result down into a byte array.
+	// http://stackoverflow.com/questions/3784263/converting-an-int-into-a-4-byte-char-array-c
+	byte intbuffer[2];
+	intbuffer[0] = (result >> 8) & 0xFF;
+	intbuffer[1] = result & 0xFF;
+
+	byte writer[] = {error_flag,command,intbuffer[0],intbuffer[1]};
+
+
+	Wire.write(writer,4);
+
+	// Now we have to reset errors, otherwise, we can get stuck.
+	error_flag = 0;
+
 }
+
 
 // --------------------------------------------------------------------------------
 // -- receiveData : Event to handle incoming data, e.g. commands from the master.
-// The master will write us a 5-byte array, followed by an new-line (0x0A) character.
-// The first three bytes denote the command to send, and the next two bytes are for parameters to that command.
+// The master will write us a 3-byte array, followed by an new-line (0x0A) character --> the master will make a request after sending 0x0A
+// The first byte is the command, the second two bytes are parameters (and the last is end-of-line)
 
 void receiveData(int byteCount){
+
+	byte buffer_index = 0;	// The Index for writing to the buffer
 
 	while(Wire.available()) {
 
 		// Let's get that byte.
-		unsigned char in = Wire.read();
+		byte inbyte = Wire.read();
 		
+		// Always assume command is incomplete, mark complete only on receipt of end of command
+		command_complete = 0;
+		if (DEBUG) {
+			Serial.println("over set");
+			Serial.println(command_complete);
+		}
+
 		// What index are we reading?
-		switch (param_index) {
+		switch (buffer_index) {
 
 			case 0:
 				// Ok, this is the first index. If it's end-of-line, it's the end of the command.
-				if (in != END_OF_COMMAND) {
+				if (inbyte != END_OF_COMMAND) {
 					// It's a command, store that.
-					command = in;
+					command = inbyte;
 				} else {
-					// That's good, it's the end of the command, we'll keep that as a success.
-					command_error_flag = false;
+					// That's good, it's the end of the command.
+					// Let's note that we completely got the command.
+					command_complete = 1;
+					
+					if (DEBUG) {
+						Serial.println("inner set");
+						Serial.println(command_complete);
+					}
 				}
 
 			default:
 				// If the buffer is not yet full, we're going to populate it.
-				if (param_index < PARAMETERS_MAX) {
+				if (buffer_index < MAX_COMMAND_PARAMETERS) {
 
 					// Place a byte into the buffer with each read, and increment the index at which it is placed.
 					// We subtract one to account for the command at position 0.
-					param_buffer[param_index-1] = in;
+					param_buffer[buffer_index-1] = inbyte;
 
 				} else {
 					// Not bueno. That's a buffer overflow.
-					command_error_flag = true;
+					error_flag = ERR_BUFFER_OVERFLOW;
 				}
 				break;
 
 		}
 
 		// We're done processing that byte, increment in the parameter index.
-		param_index++;
+		buffer_index++;
 
 	}
 
-	// No more bytes available, we'll reset the buffer index.
-	param_index = 0;
-
-}
-
-void fillRequest() {
-
-	// Let's just echo back the command and params now.
-	unsigned char writer[] = {command,param_buffer[0],param_buffer[1]};
-	Wire.write(writer,3);
+	// No more bytes available, we'll reset the buffer index (redundant)
+	buffer_index = 0;
 
 }
 
 
-// the loop routine runs over and over again forever:
+// the heart of the matter, the loop routine.
+
 void loop() {
 	delay(2000);
 	//digitalWrite(PIN_RASPI_TRANSISTOR, HIGH);   // turn the LED on (HIGH is the voltage level)
@@ -152,10 +224,36 @@ void loop() {
 		boolean ignition = digitalRead(PIN_IGNITION);
 
 		if (DEBUG) {
-			Serial.println("Value of ignition"); 
-			Serial.println(ignition); 
+			// Serial.println("Value of ignition"); 
+			// Serial.println(ignition); 
 		}
     
   	}
 
+}
+
+// the infamous setup routine.
+void setup() {
+
+	// initialize the pin to turn the relay on, as an output.
+	pinMode(PIN_RASPI_RELAY, OUTPUT);
+
+	// Here's our debug LED, it's an output.
+	pinMode(PIN_DEBUG_LED, OUTPUT);
+
+	// listen on the ignition, as input
+	pinMode(PIN_IGNITION, INPUT);
+
+	if (DEBUG) {
+	  Serial.begin(9600);
+	}
+
+	// Initialize i2c, give it the address, and the methods to call on it's events.
+	Wire.begin(I2C_ADDRESS);	
+	Wire.onReceive(receiveData);
+	Wire.onRequest(fillRequest);
+
+	// Trying an init on the error flag.
+	error_flag = 0;
+  
 }
